2fcfb29387afb16c90eeaff080414c862848858d
Rezfix <44650563+Rezfix@users.noreply.github.com>
2018-11-09 01:02:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.user.setGame('نايمم لا تزعجني','https://www.twitch.tv/Rezfix');
+    client.user.setGame('Sleeping... نايم','https://www.twitch.tv/Rezfix');
     console.log('---------------');
     console.log(' You Is Online')
     console.log('---------------')
