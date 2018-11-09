a51c5b4e3c04d147ced6ee3126c63e8a81c9d3c5
Rezfix <44650563+Rezfix@users.noreply.github.com>
2018-11-09 20:45:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.user.setGame('RezfixServer The Best ','https://www.twitch.tv/Rezfix');
+    client.user.setGame('i Love iR26RR','https://www.twitch.tv/Rezfix');
     console.log('---------------');
     console.log('iR25RR Online')
     console.log('---------------')
@@ -11,7 +11,7 @@ client.on('ready', () => {
 client.login(process.env.BOT_TOKEN);
 
 client.on('ready', () => {
-    client.user.setGame('RezfixServer The Best ','https://www.twitch.tv/Rezfix');
+    client.user.setGame('i Love iR25RR','https://www.twitch.tv/Rezfix');
     console.log('---------------');
     console.log('iR26RR Online')
     console.log('---------------')
