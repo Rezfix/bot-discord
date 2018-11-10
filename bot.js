9064fc21e43aab0efa7fd98df88f93d51d6fcfb8
Rezfix <44650563+Rezfix@users.noreply.github.com>
2018-11-10 17:10:19 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,23 +29,3 @@ iTaly.on('ready', () => {
   });
 
 iTaly.login(process.env.BOT_TOK);
-const HeyIm = new Discord.Client();
-
-HeyIm.on('ready', () => {
-   HeyIm.user.setGame('Rezfix','https://www.twitch.tv/Rezfix');
-    console.log('---------------');
-    console.log('HeyIm Online')
-    console.log('---------------')
-  });
-
-HeyIm.login(process.env.BOT_TOK);
-const AbuA = new Discord.Client();
-
-AbuA.on('ready', () => {
-   AbuA.user.setGame('Rezfix','https://www.twitch.tv/Rezfix');
-    console.log('---------------');
-    console.log('AbuA Online')
-    console.log('---------------')
-  });
-
-AbuA.login(process.env.BOT_TOK);
