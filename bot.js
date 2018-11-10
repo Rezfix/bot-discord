35f804489a7b7ffccf3723f5762974b7880643d3
Rezfix <44650563+Rezfix@users.noreply.github.com>
2018-11-10 14:58:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,24 +1,24 @@
 const Discord = require("discord.js");
 const iR25RR = new Discord.Client();
 
-iR25RR.on('ready', () => {
-    iR25RR.user.setGame('i Love iR26RR','https://www.twitch.tv/Rezfix');
+Omar.on('ready', () => {
+    Omar.user.setGame('i Love iR26RR','https://www.twitch.tv/Rezfix');
     console.log('---------------');
     console.log('iR25RR Online')
     console.log('---------------')
   });
 
-iR25RR.login(process.env.BOT_TOKEN);
-const iR26RR = new Discord.Client();
+Omar.login(process.env.BOT_TOKEN);
+const Spaiky = new Discord.Client();
 
-iR26RR.on('ready', () => {
-   iR26RR.user.setGame('i Love iR25RR','https://www.twitch.tv/Rezfix');
+Spaiky.on('ready', () => {
+   Spaiky.user.setGame('i Love iR25RR','https://www.twitch.tv/Rezfix');
     console.log('---------------');
     console.log('iR26RR Online')
     console.log('---------------')
   });
 
-iR26RR.login(process.env.BOT_TOKE);
+Spaiky.login(process.env.BOT_TOKE);
 const iR26RR = new Discord.Client();
 
 iTaly.on('ready', () => {
