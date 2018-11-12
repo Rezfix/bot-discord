b9c82d56923095f97184c1a474b129b7ce2b1972
Rezfix <44650563+Rezfix@users.noreply.github.com>
2018-11-12 21:39:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -49,13 +49,3 @@ AbuA.on('ready', () => {
   });
 
 AbuA.login(process.env.ABUA);
-const Sfa7 = new Discord.Client();
-
-Sfa7.on('ready', () => {
-   Sfa7.user.setGame('#Rezfix_i love','https://www.twitch.tv/Rezfix');
-    console.log('---------------');
-    console.log('Sfa7 Online')
-    console.log('---------------')
-  });
-
-Sfa7.login(process.env.SFA7);
